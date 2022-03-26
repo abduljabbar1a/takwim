5b7985c13d98f9d0c20fb6f6bcdf3fcf492057a9
abduljabbar1a <102430847+abduljabbar1a@users.noreply.github.com>
2022-03-26 21:32:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,6 @@ var token = '5266816142:AAHG9RC8tQ4j57Sy0s1ae94hAYI1nIoWxSQ';
 var bot = new TelegramBot(token, {polling: true});
 bot.on("polling_error", (err) => console.log(err));
 
-
 var linke= 'https://www.youtube.com/watch?v=';
 bot.on("message",function(msg){
 var monn = msg.text;
@@ -13,12 +12,7 @@ var monn = msg.text;
    
 let index = memb.indexOf(monn);
 if(index>-1){
-  var pag= monn.slice(0,nok);
-  var que= monn.slice(nok+1,nok+3);
-  
-  var rep= ' التمرين رقم'+que+' في الصفحة رقم ' + pag;    
-  
-  var tito='اضغط لمشاهدة حل التمرين';
+    
   if(msg.text == '/start'){
   rep = 'يسعدنا إنضمامك الى مجموعة حل تمارين الهندسة';
   tito='اضغط للاشتراك في المجموعة '};
