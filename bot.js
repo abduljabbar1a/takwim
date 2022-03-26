323e999047973b7646fab1b553c273cc9dc4b0d8
abduljabbar1a <102430847+abduljabbar1a@users.noreply.github.com>
2022-03-26 21:07:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 
 var TelegramBot = require('node-telegram-bot-api');
 
-var token = '1468220555:AAEXvDaC7Wx5HN4RZ2Vgm5TInUnUjKuLjhQ';
+var token = '5266816142:AAHG9RC8tQ4j57Sy0s1ae94hAYI1nIoWxSQ';
 var bot = new TelegramBot(token, {polling: true});
 bot.on("polling_error", (err) => console.log(err));
 
